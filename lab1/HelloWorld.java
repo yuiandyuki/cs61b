f8c21dfeeca4a9bb97ee24e73853eb8f65dbf0a0
lsh <lsh@lshdeMacBook-Air.local>
2023-06-01 10:29:19 +0800

    modified HelloWorld

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+	int a = 1;
         System.out.println("Hello world!");
     }
 }
